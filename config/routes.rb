64c6898ce137136c8e4ee6e993bac04dbaa0e716
Nick Raccioppi <nick@logitek.me>
2015-12-02 17:48:13 -0800

    Tuesdays Updates

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   post '/carted_products', to: 'carted_products#create'
 
   post '/orders', to: 'orders#create'
+
+  get '/orders/:id', to: 'orders#show'
   
   # The priority is based upon order of creation: first created -> highest priority.  
   # See how all your routes lay out with "rake routes".
